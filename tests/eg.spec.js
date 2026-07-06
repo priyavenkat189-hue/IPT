c1bda29bc434cf0d4c2bd8f896182e0750615fe2
priyavenkat189-hue <priyavenkat189@gmail.com>
2026-07-06 15:54:55 +0530

    changes

--- tests/eg.spec.js
+++ tests/eg.spec.js
@@ -2,9 +2,9 @@ import {test} from '@playwright/test';
 test('open a url',async({page})=>{
   await page.goto('https://www.amazon.in/');
   await page.wait for TIMEOUT(3000);
-await page.goto('https://www.flipkart.in/');
+await page.goto('https://www.instagram.in/');
   await page.wait for TIMEOUT(3000);
-  await 
+  
 
 });
 
